af8238362dfffb3e4efe5f7d6021d77fb3c8f50e
Sarthak Badgujar <sarthakbadgujar2@gmail.com>
2023-04-12 20:33:44 +0530

    Prisma module added with DB connectivity, simple API added for auth signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Request } from 'express'
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto/auth.dto";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authservice: AuthService){}
 
     @Post('signup')
-    signup(){
+    signup(@Body('email') email: string, @Body('password') password: string){
+        console.log(email)
         return this.authservice.signup()
     }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { User,Bookmark } from '@prisma/client'
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AuthService{
+
+    constructor(private prisma: PrismaService){}
 
     signin(){
         return { message: "Hello You are signed in" };
