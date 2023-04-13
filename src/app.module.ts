b26818a9244eb14b6dc99cecdb411cb224838789
SarthakB <sarthakb@winjit.com>
2023-04-13 15:12:35 +0530

    added dto with validation for req body

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, ParseIntPipe } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
@@ -8,8 +8,8 @@ export class AuthController {
   constructor(private authservice: AuthService) {}
 
   @Post('signup')
-  signup(@Body('email') email: string, @Body('password') password: string) {
-    console.log(email);
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
     return this.authservice.signup();
   }
 
